55af5939d78dc8160f1469c9cba2e3f609a27c4e
Maja <zujana@gmail.com>
2021-03-20 23:05:44 -0400

    Navigation function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hi! Thanks for visiting my Portfolio Site")
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
